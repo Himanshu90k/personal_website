e31cea21e5fbe45d8510a2ae284f175d42d54f8e
Himanshu Rawat <himanshurawat90k@gmail.com>
2025-02-09 13:08:15 +0530

    add backend skills

---
+++ app/Components/icons/MongoIcon.tsx
@@ -0,0 +1,46 @@
+const MongoIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="68" height="129" viewBox="0 0 68 129" fill="none">
+            <g filter="url(#filter0_d_95_186)">
+                <path d="M36.4587 120L33.4765 119.013C33.4765 119.013 33.8736 103.949 28.4134 102.89C24.8395 98.7356 28.9416 -74.476 42.0062 102.299C39.5274 103.555 37.6308 105.718 36.7129 108.337C36.2209 112.205 36.1357 116.114 36.4587 120Z" fill="url(#paint0_linear_95_186)" />
+                <path d="M38.0709 105.447C46.0113 99.48 52.0922 91.3856 55.6069 82.1049C59.1217 72.8243 59.9258 62.7383 57.926 53.0194C52.0886 27.311 38.2655 18.8592 36.7764 15.6323C35.4916 13.6229 34.3949 11.4996 33.5003 9.28939L34.6003 80.9201C34.6003 80.9201 32.3209 102.811 38.0709 105.447Z" fill="url(#paint1_linear_95_186)" />
+                <path d="M31.9596 106.399C31.9596 106.399 7.65297 89.8714 9.05871 60.6348C9.18984 51.8208 11.2142 43.1375 14.995 35.1715C18.7759 27.2055 24.2251 20.1422 30.9747 14.4588C31.7635 13.7898 32.3902 12.9511 32.8077 12.0056C33.2251 11.0602 33.4226 10.0326 33.3851 9C34.8981 12.2507 34.6519 57.5348 34.8107 62.831C35.4262 83.4334 33.6631 102.51 31.9596 106.399Z" fill="url(#paint2_linear_95_186)" />
+            </g>
+            <defs>
+                <filter id="filter0_d_95_186" x="0" y="0" width="68" height="129" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                    <feOffset />
+                    <feGaussianBlur stdDeviation="4.5" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix type="matrix" values="0 0 0 0 0.278431 0 0 0 0 0.647059 0 0 0 0 0.286275 0 0 0 1 0" />
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_95_186" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_95_186" result="shape" />
+                </filter>
+                <linearGradient id="paint0_linear_95_186" x1="-27.3224" y1="57.6618" x2="36.9881" y2="122.12" gradientUnits="userSpaceOnUse">
+                    <stop offset="0.231" stopColor="#999875" />
+                    <stop offset="0.563" stopColor="#9B9977" />
+                    <stop offset="0.683" stopColor="#A09F7E" />
+                    <stop offset="0.768" stopColor="#A9A889" />
+                    <stop offset="0.837" stopColor="#B7B69A" />
+                    <stop offset="0.896" stopColor="#C9C7B0" />
+                    <stop offset="0.948" stopColor="#DEDDCB" />
+                    <stop offset="0.994" stopColor="#F8F6EB" />
+                    <stop offset="1" stopColor="#FBF9EF" />
+                </linearGradient>
+                <linearGradient id="paint1_linear_95_186" x1="0.138686" y1="8.40915" x2="10.5727" y2="119.269" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#48A547" />
+                    <stop offset="1" stopColor="#3F9143" />
+                </linearGradient>
+                <linearGradient id="paint2_linear_95_186" x1="6.96786" y1="63.5288" x2="47.6196" y2="49.8915" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#41A247" />
+                    <stop offset="0.352" stopColor="#4BA74B" />
+                    <stop offset="0.956" stopColor="#67B554" />
+                    <stop offset="1" stopColor="#69B655" />
+                </linearGradient>
+            </defs>
+        </svg>
+    )
+};
+
+export default MongoIcon;
--- app/Sections/SkillsSection.tsx
+++ app/Sections/SkillsSection.tsx
@@ -6,6 +6,11 @@ import HtmlCard from "../Components/skill_cards/HtmlCard";
 import CssCard from "../Components/skill_cards/CssCard";
 import TailwindCard from "../Components/skill_cards/TailwindCard";
 import NextJsCard from "../Components/skill_cards/NextJsCard";
+import GoIcon from "../Components/icons/GoIcon";
+import SqlIcon from "../Components/icons/SqlIcon";
+import NodeIcon from "../Components/icons/NodeIcon";
+import MongoIcon from "../Components/icons/MongoIcon";
+import Arrow from "../Components/icons/Arrow";
 
 
 const SkillsSection = () => {
@@ -34,6 +39,79 @@ const SkillsSection = () => {
                 {/* line */}
                 <div className="w-2 h-260 bg-white shadow-line-shadow"></div>
                 <GlowingCircle px={9} sign="-" />
+            </div>
+
+            {/* Backend Skills */}
+            <div className="absolute bottom-[5%] left-[15%] w-3/4 flex justify-center items-center h-1/3 p-2">
+                {/* backend circle */}
+                <div className="w-82 h-82 flex items-center justify-center shadow-backend-shadow rounded-12-xl border-12px backend-border border-transparent"
+                >
+                    <h2 className="font-josefin text-white text-6xl italic font-bold">backend</h2>
+                </div>
+
+                {/* go circle */}
+                <div className="absolute top-[5%] right-0 flex items-center gap-4">
+                    <div className="w-50 h-50 flex justify-center items-center go-border border-8px border-transparent shadow-go-shadow rounded-10-xl">
+                        <GoIcon />
+                    </div>
+
+                    <div className="w-fit">
+                        <h3 className="font-josefin text-white text-4xl font-bold">Go</h3>
+                        <hr className="w-full border-2" />
+                        <div className="flex gap-1 pt-1 items-center">
+                            <Arrow />
+                            <p className="font-inter text-sm font-medium opacity-50 text-white">08</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* sql circle */}
+                <div className="absolute top-[15%] left-0 flex items-center gap-4">
+                    <div className="w-fit">
+                        <h3 className="font-josefin text-white text-4xl font-bold">SQL</h3>
+                        <hr className="w-full border-2" />
+                        <div className="flex gap-1 pt-1 items-center">
+                            <Arrow />
+                            <p className="font-inter text-sm font-medium opacity-50 text-white">09</p>
+                        </div>
+                    </div>
+
+                    <div className="w-50 h-50 flex justify-center items-center border-8px border-transparent rounded-10-xl shadow-sql-shadow sql-border">
+                        <SqlIcon />
+                    </div>
+                </div>
+
+                {/* Node circle */}
+                <div className="absolute bottom-[15%] right-0 flex items-center gap-4">
+                    <div className="w-50 h-50 flex justify-center items-center border-8px border-transparent rounded-10-xl shadow-node-shadow node-border">
+                        <NodeIcon />
+                    </div>
+
+                    <div className="w-fit">
+                        <h3 className="font-josefin text-white text-4xl font-bold">Node.js</h3>
+                        <hr className="w-full border-2" />
+                        <div className="flex gap-1 pt-1 items-center">
+                            <Arrow />
+                            <p className="font-inter text-sm font-medium opacity-50 text-white">10</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Node circle */}
+                <div className="absolute bottom-[5%] left-0 flex items-center gap-4">
+                <div className="w-fit">
+                        <h3 className="font-josefin text-white text-4xl font-bold">MongoDB</h3>
+                        <hr className="w-full border-2" />
+                        <div className="flex gap-1 pt-1 items-center">
+                            <Arrow />
+                            <p className="font-inter text-sm font-medium opacity-50 text-white">11</p>
+                        </div>
+                    </div>
+
+                    <div className="w-50 h-50 flex justify-center items-center border-8px border-transparent rounded-10-xl shadow-mongo-shadow mongo-border">
+                        <MongoIcon />
+                    </div>
+                </div>
             </div>
         </section>
     )
